43832cfa51a96732a1c1efed7121d0082d87aee9
airRnot1106 <yngw.rintaro@gmail.com>
2021-09-22 17:54:42 +0900

    app.tsを追加

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { Database } from './index';
+
+const app: express.Express = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./updateHistory.html'));
+
+require('./client');
+
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+  }
+);
+
+app.listen(3000, () => {
+  console.log('Start on port 3000.');
+  Database;
+});
+
+app.get('/', function (req, res) {
+  res.send('connected');
+});
